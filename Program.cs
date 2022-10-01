9c9068c579285391372c1b6599e9ebd0c94b9ad5
Bruno Eduardo <jogar1203@gmail.com>
2022-10-01 18:12:38 -0300

    append

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿    Console.WriteLine("Digite um nome: " );
+
+string nome = Console.ReadLine();
+
+Console.WriteLine(nome);
+
+while (nome.Length >0)
+{
+    Console.WriteLine("Digite a letra a ser removida: " );
+    string letra = Console.ReadLine();
+    nome = nome.Replace(letra, "");
+    Console.WriteLine(nome);
+    Console.WriteLine(nome.Trim());
+}
